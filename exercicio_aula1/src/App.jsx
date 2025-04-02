1a1111f1db2abdd7fed61f9ace90a4a23cb4499f
ViniciusSilas01 <vinicius2012silas@gmail.com>
2025-04-02 19:35:54 -0300

    nutrindo

--- exercicio_aula1/src/App.jsx
+++ exercicio_aula1/src/App.jsx
@@ -13,15 +13,15 @@ const App = () => {
 
         <h3>
          <Adicao num1={1} num2={1} />     
-         <Subtracao num1={6} num2={4} />  
+         <Subtracao num1={6} num2={6} />  
          <Multiplicacao num1={1} num2={2} />    
-         <Divisao num1={8} num2={4} />
+         <Divisao num1={10} num2={2} />
         </h3>
          
         <h1>Exercício 2 da aula 1</h1>
 
         <h6>
-         <PrecisoEstudar nomeDaTecnologia={"inglês"}></PrecisoEstudar>
+         <PrecisoEstudar nomeDaTecnologia={"JavaScript"}></PrecisoEstudar>
         </h6>
 
         </div>
